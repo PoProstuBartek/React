e07b34295b651f0601c2daaf7009119674b0e920
Bartłomiej Szczotkowski <szczotkowski.b@gmail.com>
2022-01-20 18:57:47 +0100

    Add search columns functionality

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,16 +1,25 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { updateSearchstring } from '../../redux/store';
 
 const SearchForm = () => {
-    return (
-      <form className={styles.searchForm}>
-        <TextInput placeholder="Search..."/>
-        <Button>
-          <span className="fa fa-search" />
-        </Button>
-      </form>
-    );
+  const dispatch = useDispatch();
+  const [searchString, setSearchString] = useState('')
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(updateSearchstring({searchString}));
+  }
+  return (
+    <form className={styles.searchForm} onSubmit={handleSubmit}>
+      <TextInput placeholder="Search..." value={searchString} onChange={e => setSearchString(e.target.value)}/>
+      <Button>
+        <span className="fa fa-search" />
+      </Button>
+    </form>
+  );
   };
 
   export default SearchForm;
---
+++ src/utils/strContains.js
@@ -0,0 +1,5 @@
+const strContains = (firstString, secondString) => {
+  return firstString.toLowerCase().includes(secondString.toLowerCase());
+}
+
+export default strContains;
